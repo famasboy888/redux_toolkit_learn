73b7b505be4eb86184f43de96fd0ddb96fa6c97b
famasboy888 <famasboy88@gmail.com>
2024-05-04 21:45:54 +0800

    Added RTK pattern

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# redux_toolkit_learn
-# redux_toolkit_learn
+# Redux ToolKit
+
+The basic pattern of implementing Redux Toolkit:
+
+1) Create and follow folder structure
+2) Create `slice`
+3) Inside the `slice`. Follow implementation [here]()
+4) Import the slice and `configureStore` with reducers in `app/store.js`
+5) Import store, subscribe to store and dispatch actions from `index.js` 
---
+++ index.js
@@ -0,0 +1,17 @@
+const store = require('./app/store');
+const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const iceCreamAction = require('./features/icecream/iceCreamSlice').iceCreamAction;
+
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(()=>{
+    console.log('Update state', store.getState());
+})
+
+store.dispatch(cakeActions.ordered(2));
+store.dispatch(cakeActions.restocked(9));
+
+store.dispatch(iceCreamAction.ordered(5));
+store.dispatch(iceCreamAction.restocked(30));
+
+unsubscribe();
